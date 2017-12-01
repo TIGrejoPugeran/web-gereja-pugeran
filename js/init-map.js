9550444fcd2767a061875309364dba0e5ed09735
Agustinus Wikrama <agustinuswikrama@gmail.com>
2017-12-01 19:54:27 +0700

    Fix jadwal misa

--- js/init-map.js
+++ js/init-map.js
@@ -44,3 +44,16 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementsByTagName('head')[0].appendChild(js_file);
   }
 });
+
+$(function(){
+    var navMain = $(".navbar-collapse"); // avoid dependency on #id
+    // "a:not([data-toggle])" - to avoid issues caused
+    // when you have dropdown inside navbar
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+
+    var shiftWindow = function() { scrollBy(0, -50) };
+    if (location.hash) shiftWindow();
+      window.addEventListener("hashchange", shiftWindow);
+});
